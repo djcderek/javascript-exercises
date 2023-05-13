a8504de6b941f8ec8ef5c06d24a009b0c6b2fee8
Derek Chan <djcderek@gmail.com>
2023-05-12 21:58:55 -0700

    complete first 4 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(myString) {
+    let reverseString = '';
+    for (let i = 0; i < myString.length; i++) {
+        let currentLetter = myString.slice(i, i + 1);
+        reverseString = currentLetter + reverseString;
+    }
 
+    return reverseString
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...myArray) {
+    let newArray = myArray.filter((element) => !Array.isArray(element));
+    let singleArray = myArray.shift();
+    let finalArray = [];
+    for (const element of singleArray) {
+        if (!newArray.includes(element)) {
+            finalArray.push(element)
+        }
+    }
+    return finalArray
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
