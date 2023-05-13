9241442c7bf01d6298c0f3a42fc0620d5a1dea9e
Derek Chan <djcderek@gmail.com>
2023-05-12 23:25:48 -0700

    complete all assignments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let startNum = a;
+    let endNum = b;
 
+    if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+
+    if (typeof a != "number" || typeof b != "number") {
+        return "ERROR"
+    }
+
+    if (startNum > endNum) {
+        let temp = startNum;
+        startNum = endNum;
+        endNum = temp;
+    }
+
+    for (i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numF) {
+  return Number(((numF - 32)*(5/9)).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numC) {
+  return Number((numC*(9/5) + 32).toFixed(1))
 };
 
 // Do not edit below this line
