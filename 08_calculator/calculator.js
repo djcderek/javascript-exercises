e7efc49f9073fc6a8dc67d085b8f2b38b2f26814
Derek Chan <djcderek@gmail.com>
2023-05-21 23:30:04 -0700

    Complete calculator assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  return numArray.reduce((sum, num) => {
+    return sum + num
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((mult, num) => {
+    return mult * num
+  })
 };
 
-const power = function() {
-	
+const power = function(a , b) {
+  return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1
+  }
+  let total = a
+  for (let i = a; i > 1; i--) {
+    total = total * (i-1)
+  }
+  return total
 };
 
 // Do not edit below this line
