2a6fa4509eaed985f55018f7ce08a02721149f1c
Derek Chan <djcderek@gmail.com>
2023-05-22 10:34:26 -0700

    Complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let number = Number(index);
+    if (number < 0) {
+        return "OOPS"
+    }
+    let sequence = [1,1];
+    for (let i = 0; i < number - 2; i++) {
+        let nextNum = sequence[i] + sequence[i + 1];
+        sequence[i + 2] = nextNum;
+    }
+    return sequence[sequence.length - 1]
 };
 
 // Do not edit below this line
