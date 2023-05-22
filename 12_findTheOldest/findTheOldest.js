14e7a466caf0704ac4fdaa1e68d6d35c6fdb24af
Derek Chan <djcderek@gmail.com>
2023-05-22 12:55:51 -0700

    Complete findTheOldest assignment the final one

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sortedPeople =  people.sort((a,b) => {
+        if (a.yearOfDeath === undefined) a.yearOfDeath = 2023
+        if (b.yearOfDeath === undefined) b.yearOfDeath = 2023
+        if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+            return -1
+        } else {
+            return 1
+        }
+    })
 
+    return sortedPeople[0]
 };
 
 // Do not edit below this line
