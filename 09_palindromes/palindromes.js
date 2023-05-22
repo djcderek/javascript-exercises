eedc086114871f4f67d461a87e302bab70efaf1f
Derek Chan <djcderek@gmail.com>
2023-05-22 10:09:31 -0700

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    const palindromeArray = palindrome.split('');
+    const noSpaces = palindromeArray.filter(letter => ((letter !== ' ') && (letter.toUpperCase() !== letter.toLowerCase())))
+    const upperCase = noSpaces.map((letter) => letter.toUpperCase())
+    for (let i = 0; i < upperCase.length - 1; i++) {
+        if (upperCase[i] !== upperCase[upperCase.length-i - 1]) {
+            console.log(upperCase[i], upperCase[upperCase.length - i - 1])
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
